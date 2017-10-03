27b6a2f1905603e27a5fd36d91c523195a47faa2
BryanYu <ak78912@hotmail.com>
2017-10-03 22:13:56 +0800

    Decorator - First Version

---
+++ Decorator/Persons/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Persons
+{
+    public class Person
+    {
+        private string _name;
+
+        public Person(string name)
+        {
+            this._name = name;
+        }
+
+        public void WearTShirts()
+        {
+            Console.WriteLine("大T");
+        }
+
+        public void WearBigTrouser()
+        {
+            Console.WriteLine("垮褲");
+        }
+
+        public void WearSneakers()
+        {
+            Console.WriteLine("破球鞋");
+        }
+
+        public void WearSuit()
+        {
+            Console.WriteLine("西裝");
+        }
+
+        public void WearTie()
+        {
+            Console.WriteLine("領帶");
+        }
+
+        public void WearLeatherShoes()
+        {
+            Console.WriteLine("皮鞋");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("裝扮的{0}", this._name);
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,30 @@
+﻿using Decorator.Persons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person("Bryan");
+
+            Console.WriteLine("第一種裝扮");
+            person.WearTShirts();
+            person.WearBigTrouser();
+            person.WearSneakers();
+            person.Show();
+
+            Console.WriteLine("第二種裝扮");
+            person.WearSuit();
+            person.WearTie();
+            person.WearLeatherShoes();
+            person.Show();
+            Console.Read();
+        }
+    }
+}
