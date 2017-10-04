05f5b882eceac886df226272a37c52329485bfa5
BryanYu <ak78912@hotmail.com>
2017-10-04 15:22:35 +0800

    Proxy - First Version

---
+++ Proxy/Program.cs
@@ -0,0 +1,23 @@
+﻿using Proxy.Pursuits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var girl = new SchoolGirl() { Name = "正妹" };
+            var pursuit = new Pursuit(girl);
+            pursuit.GiveDolls();
+            pursuit.GiveFlowers();
+            pursuit.GiveChocolate();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Proxy/Pursuits/Pursuit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.Pursuits
+{
+    public class Pursuit
+    {
+        private SchoolGirl _girl;
+
+        public Pursuit(SchoolGirl girl)
+        {
+            this._girl = girl;
+        }
+
+        public void GiveDolls()
+        {
+            Console.WriteLine(this._girl.Name + "送你洋娃娃");
+        }
+
+        public void GiveFlowers()
+        {
+            Console.WriteLine(this._girl.Name + "送你鮮花");
+        }
+
+        public void GiveChocolate()
+        {
+            Console.WriteLine(this._girl.Name + "送你巧克力");
+        }
+    }
+}
