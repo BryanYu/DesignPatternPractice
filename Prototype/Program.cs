fde5ca61dba12138c2b67957e1a749c09c52ee5c
BryanYu <ak78912@hotmail.com>
2017-10-04 16:34:10 +0800

    Prototype - First Version

---
+++ Prototype/Program.cs
@@ -0,0 +1,33 @@
+﻿using Prototype.Resumes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var resume = new Resume("Bryan");
+            resume.SetPersonalInfo("男", "28");
+            resume.SetWorkExperience("1998-2000", "XX公司");
+
+            var resume2 = new Resume("Bryan2");
+            resume2.SetPersonalInfo("男", "25");
+            resume2.SetWorkExperience("1998-2006", "YY公司");
+
+            var resume3 = new Resume("Bryan3");
+            resume3.SetPersonalInfo("男", "25");
+            resume3.SetWorkExperience("1998-2005", "ZZ公司");
+
+            resume.Display();
+            resume2.Display();
+            resume3.Display();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Prototype/Resumes/Resume.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Resumes
+{
+    internal class Resume
+    {
+        private string _name;
+
+        private string _sex;
+
+        private string _age;
+
+        private string _timeArea;
+
+        private string _company;
+
+        public Resume(string name)
+        {
+            this._name = name;
+        }
+
+        public void SetPersonalInfo(string sex, string age)
+        {
+            this._sex = sex;
+            this._age = age;
+        }
+
+        public void SetWorkExperience(string timeArea, string company)
+        {
+            this._timeArea = timeArea;
+            this._company = company;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("{0} {1} {2}", this._name, this._sex, this._age);
+            Console.WriteLine("工作經歷：{0} {1}", this._timeArea, this._company);
+        }
+    }
+}
