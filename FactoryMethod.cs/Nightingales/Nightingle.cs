b3780f94550a9d8e567fc72782ec4d25228af0f8
BryanYu <ak78912@hotmail.com>
2017-10-04 15:58:21 +0800

    FactoryMethod - First Version

---
+++ FactoryMethod.cs/Nightingales/Nightingle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.cs.Nightingales
+{
+    internal class Nightingle
+    {
+        public void Sweep()
+        {
+            Console.WriteLine("掃地");
+        }
+
+        public void Wash()
+        {
+            Console.WriteLine("洗衣");
+        }
+
+        public void BuyRice()
+        {
+            Console.WriteLine("買米");
+        }
+    }
+}
---
+++ FactoryMethod.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using FactoryMethod.cs.Nightingales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var nightingle = new Undergraduate();
+            nightingle.BuyRice();
+            nightingle.Sweep();
+            nightingle.Wash();
+        }
+    }
+}
