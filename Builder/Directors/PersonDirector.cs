cd648ef09e3afae6f489adfa9599451d0400430f
BryanYu <ak78912@hotmail.com>
2017-10-10 16:49:21 +0800

    Builder - Add Director Class

---
+++ Builder/Directors/PersonDirector.cs
@@ -0,0 +1,29 @@
+﻿using Builder.PersonBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Directors
+{
+    internal class PersonDirector
+    {
+        private PersonBuilder _builder;
+
+        public PersonDirector(PersonBuilder builder)
+        {
+            this._builder = builder;
+        }
+
+        public void CreatePerson()
+        {
+            this._builder.BuildHead();
+            this._builder.BuildBody();
+            this._builder.BuildArmLeft();
+            this._builder.BuildArmRight();
+            this._builder.BuildLegLeft();
+            this._builder.BuildLegRight();
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using Builder.Directors;
+using Builder.PersonBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var thinBulider = new PersonThinBuilder();
+            var thinDirector = new PersonDirector(thinBulider);
+            thinDirector.CreatePerson();
+
+            var fatBulider = new PersonFatBuilder();
+            var fatDirector = new PersonDirector(fatBulider);
+            fatDirector.CreatePerson();
+
+            Console.Read();
+        }
+    }
+}
