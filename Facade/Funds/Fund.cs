af480eac88b3bb89d24beeb63ea225ca95af12f9
BryanYu <ak78912@hotmail.com>
2017-10-10 16:27:31 +0800

    Facade - Add Fund Class

---
+++ Facade/Funds/Fund.cs
@@ -0,0 +1,47 @@
+﻿using Facade.Stocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Funds
+{
+    internal class Fund
+    {
+        private Stock1 _stock1 { get; set; }
+        private Stock2 _stock2 { get; set; }
+        private Stock3 _stock3 { get; set; }
+
+        private NationalDebt1 _nationalDebt1 { get; set; }
+
+        private Realty1 _realty1 { get; set; }
+
+        public Fund()
+        {
+            this._stock1 = new Stock1();
+            this._stock2 = new Stock2();
+            this._stock3 = new Stock3();
+            this._nationalDebt1 = new NationalDebt1();
+            this._realty1 = new Realty1();
+        }
+
+        public void BuyFund()
+        {
+            this._stock1.Buy();
+            this._stock2.Buy();
+            this._stock3.Buy();
+            this._nationalDebt1.Buy();
+            this._realty1.Buy();
+        }
+
+        public void SellFund()
+        {
+            this._stock1.Sell();
+            this._stock2.Sell();
+            this._stock3.Sell();
+            this._nationalDebt1.Sell();
+            this._realty1.Sell();
+        }
+    }
+}
--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,4 +1,5 @@
-﻿using Facade.Stocks;
+﻿using Facade.Funds;
+using Facade.Stocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,23 +12,10 @@ namespace Facade
     {
         public static void Main(string[] args)
         {
-            var stock1 = new Stock1();
-            var stock2 = new Stock2();
-            var stock3 = new Stock3();
-            var nationalDebt1 = new NationalDebt1();
-            var realty1 = new Realty1();
-
-            stock1.Buy();
-            stock2.Buy();
-            stock3.Buy();
-            nationalDebt1.Buy();
-            realty1.Buy();
-
-            stock1.Sell();
-            stock2.Sell();
-            stock3.Sell();
-            nationalDebt1.Sell();
-            realty1.Sell();
+            var fund = new Fund();
+            fund.BuyFund();
+            fund.SellFund();
+            Console.Read();
         }
     }
 }
