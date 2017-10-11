c608527d9c48ec286ee43253f8e226a22b1d1f98
BryanYu <ak78912@hotmail.com>
2017-10-11 21:52:55 +0800

    AbstractFactory - First Version

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿using AbstractFactory.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User();
+            var sqlserverUser = new SqlserverUser();
+            sqlserverUser.Insert(user);
+            sqlserverUser.GetUser(1);
+
+            Console.Read();
+        }
+    }
+}
---
+++ AbstractFactory/Users/SqlserverUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Users
+{
+    internal class SqlserverUser
+    {
+        public void Insert(User user)
+        {
+            Console.WriteLine("SQL Server新增資料");
+        }
+
+        public User GetUser(int id)
+        {
+            Console.WriteLine("SQL Server查詢資料");
+            return null;
+        }
+    }
+}
