f20cb1237751ee311335de05dda54a76c9f24075
Bryan <d58526@gmail.com>
2017-10-14 09:57:22 +0800

    State - First Version

---
+++ State/Program.cs
@@ -0,0 +1,37 @@
+﻿using State.Works;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Work emergencyProjects = new Work();
+            emergencyProjects.Hour = 9;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 10;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 9;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 10;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 13;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 14;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 17;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Finish = false;
+            emergencyProjects.Hour = 19;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 22;
+            emergencyProjects.WriteProgram();
+            Console.Read();
+        }
+    }
+}
---
+++ State/Works/Work.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State.Works
+{
+    internal class Work
+    {
+        public int Hour { get; set; }
+
+        public bool Finish { get; set; }
+
+        public void WriteProgram()
+        {
+            if (Hour < 12)
+            {
+                Console.WriteLine($"當前時間:{Hour}點 上午工作 精神百倍");
+            }
+            else if (Hour < 13)
+            {
+                Console.WriteLine($"當前時間:{Hour}點 餓了 午飯 午休");
+            }
+            else if (Hour < 17)
+            {
+                Console.WriteLine($"當前時間:{Hour}點 下午狀態還不錯 繼續");
+            }
+            else
+            {
+                if (Finish)
+                {
+                    Console.WriteLine($"當前時間:{Hour}點 下班了");
+                }
+                else
+                {
+                    if (Hour > 21)
+                    {
+                        Console.WriteLine($"當前時間:{Hour}點 加班");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"當前時間:{Hour}點 不行了");
+                    }
+                }
+            }
+        }
+    }
+}
