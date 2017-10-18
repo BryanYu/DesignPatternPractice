33478a3e9e60f1ce47c9d5cf06c56b7a129b0a04
BryanYu <ak78912@hotmail.com>
2017-10-18 22:35:26 +0800

    Singleton - First Version

---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            if (s1 == s2)
+            {
+                Console.WriteLine("Same");
+            }
+
+            Console.Read();
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
