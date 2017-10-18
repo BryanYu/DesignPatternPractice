6c2a710241e8f5859302535ee60bbb27c227c826
BryanYu <ak78912@hotmail.com>
2017-10-18 21:54:23 +0800

    Iterator - First Version

---
+++ Iterator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IEnumerable<int> temps = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (var temp in temps)
+            {
+                Console.WriteLine(temp);
+            }
+        }
+    }
+}
