94f16f305fe04ab8bc9761a0d12e49c6712044d1
BryanYu <ak78912@hotmail.com>
2017-10-19 22:35:44 +0800

    Bridage - Add new Brand and Soft

---
+++ Bridage/Brand/HandsetBrandS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridage.Brand
+{
+    internal class HandsetBrandS : HandsetBrand
+    {
+        public override void Run()
+        {
+            this._soft.Run();
+        }
+    }
+}
---
+++ Bridage/Soft/HandsetMP3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridage.Soft
+{
+    internal class HandsetMP3 : HandSoft
+    {
+        public override void Run()
+        {
+            Console.WriteLine("MP3");
+        }
+    }
+}
