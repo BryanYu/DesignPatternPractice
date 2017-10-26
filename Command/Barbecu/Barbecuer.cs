79cbe7631bce83d2bf19a90edc58452cfb99da58
BryanYu <ak78912@hotmail.com>
2017-10-26 21:26:07 +0800

    Command - First Version

---
+++ Command/Barbecu/Barbecuer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Barbecu
+{
+    internal class Barbecuer
+    {
+        public void BakeMutton()
+        {
+            Console.WriteLine("烤羊肉串");
+        }
+
+        public void BakeChickenWing()
+        {
+            Console.WriteLine("烤雞翅");
+        }
+    }
+}
---
+++ Command/Program.cs
@@ -0,0 +1,24 @@
+﻿using Command.Barbecu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var boy = new Barbecuer();
+            boy.BakeMutton();
+            boy.BakeChickenWing();
+            boy.BakeMutton();
+            boy.BakeMutton();
+            boy.BakeMutton();
+            boy.BakeChickenWing();
+            Console.Read();
+        }
+    }
+}
