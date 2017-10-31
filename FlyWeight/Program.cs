62ee740e18b9d6553fbb9f3715a2aa7ad8f6b775
BryanYu <ak78912@hotmail.com>
2017-10-31 20:51:03 +0800

    FlyWeight - First Version

---
+++ FlyWeight/Program.cs
@@ -0,0 +1,35 @@
+﻿using FlyWeight.Site;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyWeight
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var webSite1 = new WebSite("產品展示");
+            webSite1.Use();
+
+            var webSite2 = new WebSite("產品展示");
+            webSite2.Use();
+
+            var webSite3 = new WebSite("產品展示");
+            webSite3.Use();
+
+            var webSite4 = new WebSite("部落格");
+            webSite4.Use();
+
+            var webSite5 = new WebSite("部落格");
+            webSite5.Use();
+
+            var webSite6 = new WebSite("部落格");
+            webSite6.Use();
+
+            Console.Read();
+        }
+    }
+}
---
+++ FlyWeight/Site/WebSite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyWeight.Site
+{
+    internal class WebSite
+    {
+        private string _name;
+
+        public WebSite(string name)
+        {
+            this._name = name;
+        }
+
+        public void Use()
+        {
+            Console.WriteLine($"網站分類{this._name}");
+        }
+    }
+}
